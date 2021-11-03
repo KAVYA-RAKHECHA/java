926eec1bf3bb6db894e56a4c9bb2fab0ff2422bf
KAVYA-RAKHECHA <91592144+KAVYA-RAKHECHA@users.noreply.github.com>
2021-11-03 18:12:03 +0530

    Add files via upload

---
+++ class larger.java
@@ -0,0 +1,18 @@
+class larger
+{
+    public static void main(int a, int b)
+    {
+if(a>b);
+{
+System.out.println(a+ "is the largest number");
+}
+elseif(a==b);
+{
+System.out.println("Numbers are equal");
+}
+else
+{ 
+System.out.println(b+ "is the largest number");
+}
+    }
+}
